bfc502a6fb3dfff7787eb6edbc94e578ff0c95de
kacpor2222-source <kacpor2222@gmail.com>
2026-05-13 10:05:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+
+const app = express(); // 🔥 TO BRAKOWAŁO
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("GemScope backend działa 🚀");
+});
+
 app.get("/prices", async (req, res) => {
   try {
     const response = await axios.get("https://db.biggames.io/", {
@@ -18,4 +29,10 @@ app.get("/prices", async (req, res) => {
       error: err.message
     });
   }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server działa na porcie", PORT);
 });
